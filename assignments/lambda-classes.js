2acd779d15050c1dba6c31dfee90ba0e6e9671ff
Elijah Conley <elijahandaleciael@gmail.com>
2019-04-25 17:55:24 -0400

    person and instructor is done need to do student and pm the console it

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,32 @@
 // CODE here for your Lambda Classes
+
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+  }
+speak() {
+       console.log(`Hello my is ${this.name}, I am from ${location}`);
+        }
+    };
+
+
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes);
+            this.specialty = attributes.specialty;
+            this.favLanguage = attributes.favLanguage;
+            this.location = attributes.location;
+            this.catchPhrase = attributes.catchPhrase;
+  }
+demo(subject) {
+       console.log(`Today we are learning about ${subject}`);
+        }
+         grade(student, subject) {
+            console.log(`${student.name} receives a perfect score on ${subject}`);
+             }
+         };
+      
